b176846a08fd82d0d4cc454b865c602241982b63
Ed <ed.j.flaherty@gmail.com>
2016-01-20 14:06:39 -0500

    new files

---
+++ birthday.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir /home/ed/Desktop/Happy_Birthday
+
+#echo "Happy Birthday"
--- homebackup.sh
+++ homebackup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #This script is used to backup the user's home directory.
 
-echo "Availiable home folders:"
-ls /home
-echo
-echo "Which home foler would you like to backup?:"
-read HOMEFOLDER
-echo
-
+#echo "Availiable home folders:"
+#ls /home
+#echo
+#echo "Which home foler would you like to backup?:"
+#read HOMEFOLDER
+#echo
+####################################################
+#Configuration Variables.
+####################################################
+HOMEFOLDER="ed"
 BACKUPDIR=/home
 TIMESTAMP=$(date | awk '{ print $1"_"$2"_"$3"_"$6 }')
 TARFILENAME="/var/tmp/$TIMESTAMP.$HOMEFOLDER.tar"
@@ -16,13 +19,32 @@ REMOTESERVER="10.1.10.73"
 REMOTEUSER="administrator"
 REMOTEDIR="~"
 LOGFILE="/var/tmp/homebackuplog.txt"
-
+DELDATE=$(date -d "15 day ago" '+%a_%b_%d_%Y')
+DELTARFILE="$DELDATE.$HOMEFOLDER.tar"
+DELGZIPFILE="$DELTARFILE.gz"
+####################################################
 
 cd $BACKUPDIR
+
+if [ -f $TARFILENAME ]
+then
+rm $TARFILENAME
+fi
+
 tar -cf $TARFILENAME $HOMEFOLDER
+
+if [ -f $GZIPFILENAME ]
+then
+rm $GZIPFILENAME
+fi
+
 gzip $TARFILENAME
 
-rsync $GZIPFILENAME $REMOTEUSER@$REMOTESERVER:$REMOTEDIR
+sshpass -p "southhills#" scp $GZIPFILENAME $REMOTEUSER@$REMOTESERVER:$REMOTEDIR
 
 date >> $LOGFILE
 echo "Backup complete."
+
+cd /var/tmp
+rm $DELGZIPFILE
+echo "Backups 15 days ago have been removed."
